15aac67f9c2f3fcdb0cacc5851fc0894e68e1ba5
Abhinav <abhi@Abhinavs-MacBook-Pro-783.local>
2026-07-05 14:34:06 -0700

    fix: temporarily allow all origins for debugging

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", os.getenv("FRONTEND_URL","")],  # Allow your React app's address
+    allow_origins=["*"],  # Allow your React app's address
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (POST, GET, etc.)
     allow_headers=["*"],  # Allow all security/content headers
